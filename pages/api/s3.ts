d77f901c6c70949f5fe6d41987d65eb19116d44b
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-01-30 09:13:57 -0700

    sdk installed

--- pages/api/s3.ts
+++ pages/api/s3.ts
@@ -1,10 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { randomUUID } from 'crypto'
+import S3 from "aws-sdk/client/s3";
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   res.status(200).json({ name: 'John Doe' })
 }
