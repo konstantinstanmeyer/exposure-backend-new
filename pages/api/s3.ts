0c1d9b2130c9dc300ad0215628285102d6d02b7b
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-01-30 10:19:01 -0700

    s3 uploads and permissions

--- pages/api/s3.ts
+++ pages/api/s3.ts
@@ -1,11 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { randomUUID } from 'crypto'
-import S3 from "aws-sdk/client/s3";
+import S3 from 'aws-sdk/clients/s3';
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  res.status(200).json({ name: 'John Doe' })
+const s3 = new S3({
+  apiVersion: "2006-03-01",
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECRET_KEY,
+  region: process.env.REGION,
+  signatureVersion: 'v4'
+})
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const fileType = (req.query.type as string).split('/')[1]
+
+  const Key = `${randomUUID()}.${fileType}`;
+
+  const s3Params = {
+    Bucket: process.env.BUCKET_NAME,
+    Key,
+    Expires: 60,
+    ContentType: `image/${fileType}`,
+  }
+
+  const uploadUrl = await s3.getSignedUrl('putObject', s3Params)
+
+  res.status(200).json({
+    uploadUrl,
+    key: Key,
+  });
 }
--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,18 +1,34 @@
+import axios from 'axios';
 import { ChangeEvent } from 'react';
 
 async function uploadToS3(e: ChangeEvent<HTMLFormElement>){
     const formData = new FormData(e.target);
 
-    const file = formData.get("file");
+    const file: any = formData.get("file");
 
     if (!file) return null;
 
     // @ts=ignore
 
     const fileType = encodeURIComponent(file.type);
+
+    const { data } = await axios.get(`/api/s3?type=${fileType}`);
+
+    const { uploadUrl, key } = data;
+
+    await axios.put(uploadUrl, file);
+
+    return key;
 }
 
 export default function Upload(){
+    async function handleSubmit(e: ChangeEvent<HTMLFormElement>){
+        e.preventDefault();
+
+        const key = await uploadToS3(e);
+
+        console.log(key);
+    }
 
     return (
         <div>
@@ -20,6 +36,7 @@ export default function Upload(){
                 <input type="file" accept="image/jpeg image/png" name="file"/>
                 <button type="submit">Submit</button>
             </form>
+            <img src="https://exposure-s3-bucket.s3.amazonaws.com/3ab12b92-712c-4934-9f27-aeeeff60db91.png" />
         </div>
     )
 }
