e4fea2fa6b9b465a3a97b739a058ddd3f3f3239c
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-02-05 09:43:36 -0700

    post page complete/needs routing

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch } from '../src/store'
 import { setUsername, setToken } from '@/features/auth/authSlice'
 
 export default function Navbar(){
     const dispatch = useDispatch<AppDispatch>();
+
+    const username = useSelector((state: any) =>  state.auth.username);
 
     const router = useRouter();
 
@@ -22,11 +24,9 @@ export default function Navbar(){
     return(
         <div className="h-20 w-screen bg-neutral-800 flex flex-row items-center fixed top-0 z-40">
             <img className="w-14 mx-12" src="/camera.png" />
-            <div className="w-52 ml-8 flex flex-wrap justify-center">
-                <input className="rounded-sm indent-2 p-1 !outline-none bg-gray-300" placeholder="find a category..." />
-            </div>
+            <p className="text-gray-300 font-bold text-3xl">hello{username ? `, ${username}` : "NO"}</p>
             <Link className="ml-auto mx-3 text-gray-300 font-bold mb-1 hover:bg-gray-700 transition-all duration-300 rounded-full px-3 py-1" href="/post">+</Link>
-            <Link href="/discover" className="text-gray-300 text-sm font-mono font-bold mx-5 hover:underline">discover</Link>
+            <Link href="/" className="text-gray-300 text-sm font-mono font-bold mx-5 hover:underline">home</Link>
             <Link href="/friends" className="text-gray-300 text-sm font-mono font-bold mx-5 hover:underline">suggest</Link>
             <p onClick={() => handleLogout()} className="text-gray-300 text-sm font-mono font-bold mx-5 hover:underline hover:cursor-pointer">log out</p>
             <img onClick={() => router.push(`/profile`)} className="w-10 ml-5 mr-10 rounded-full hover:cursor-pointer" src="/profile.png" />
--- pages/post.tsx
+++ pages/post.tsx
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import { ChangeEvent, useState, useEffect } from 'react';
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Navbar from "@/components/Navbar";
+import { AppDispatch } from '../src/store'
+import validate from '@/util/validateUser';
 
 async function uploadToS3(e: ChangeEvent<HTMLFormElement>){
     const formData = new FormData(e.target);
@@ -25,48 +27,90 @@ async function uploadToS3(e: ChangeEvent<HTMLFormElement>){
 }
 
 export default function Post(){
-    const [genre, setGenre] = useState("");
+    const [title, setTitle] = useState("");
     const [category, setCategory] = useState<any>("");
     const [sub, setSub] = useState<any>("");
     const [description, setDescription] = useState("");
+    const [error, setError] = useState<null | String>(null);
     const username = useSelector((state: any) =>  state.auth.username);
     const token = useSelector((state: any) =>  state.auth.token);
+
+    const dispatch = useDispatch<AppDispatch>();
 
     const router = useRouter();
 
     useEffect(() => {
-        if (!username || !token) router.push('/login');
+        const tempCategory = router.query.category;
+        const tempSub = router.query.category;
 
-        setCategory(router.query.category);
-        setSub(router.query.sub);
+        if(validate(username, token, dispatch)){
+            if (tempCategory && tempSub) {
+                setCategory(router.query.category);
+                setSub(router.query.sub);
+    
+                const categoryInput = document.querySelector("#cat-input") as HTMLInputElement;
+                const subInput = document.querySelector("#sub-input") as HTMLInputElement;
+    
+                categoryInput.disabled = true;
+                subInput.disabled = true;
+            }
+        } else {
+            router.push('/login');
+        }
     }, [])
 
     async function handleSubmit(e: ChangeEvent<HTMLFormElement>){
         e.preventDefault();
 
-        const key = await uploadToS3(e);
-        // console.log(key);
+        try {
+            const key = await uploadToS3(e);
+            // console.log(key);
 
-        const response = await axios.post('http://localhost:3001/post', {
-            genre: genre,
-            category: category,
-            description: description,
-            imageUrl: "https://exposure-s3-bucket.s3.amazonaws.com/" + key
-        }, { headers: { "Authorization": "Bearer " + localStorage.getItem('token')}});
+            const response = await axios.post('http://localhost:3001/post', {
+                title: title,
+                category: category,
+                subCategory: sub,
+                description: description,
+                imageUrl: "https://exposure-s3-bucket.s3.amazonaws.com/" + key
+            }, { headers: { "Authorization": "Bearer " + localStorage.getItem('token')}});
 
-        console.log(response);
+            console.log(response);
+        } catch(e: any){
+            setError(e.message);
+        }
     }
 
     return (
         <div className="">
             <Navbar />
-            <form onSubmit={handleSubmit}>
-                <input type="file" accept="image/jpeg image/png" name="file"/>
-                <input type="text" value={genre} onChange={e => setGenre(e.target.value)} name="genre" />
-                <input type="text" value={category} onChange={e => setCategory(e.target.value)} name="category" />
-                <input type="text" value={description} onChange={e => setDescription(e.target.value)} name="description" />
-                <button type="submit">Submit</button>
+            <form className="mt-32 w-1/4 mx-auto flex flex-col justify-center" onSubmit={handleSubmit}>
+                <div className="w-1/3 mx-auto relative my-8 flex justify-center">
+                    <input className="rounded-md indent-3 py-2 !outline-none" type="text" value={title} onChange={e => setTitle(e.target.value)} name="category" />
+                    <p className="absolute -top-[1.3rem] left-[0.15rem] text-sm text-gray-300">title</p>
+                </div>
+                <div className="w-1/3 mx-auto relative my-8 flex justify-center">
+                    <input className="rounded-md indent-3 py-2 !outline-none" id="cat-input" type="text" value={category} onChange={e => setCategory(e.target.value)} name="category" />
+                    <p className="absolute -top-[1.3rem] left-[0.15rem] text-sm text-gray-300">category</p>
+                </div>
+                <div className="w-1/3 mx-auto relative my-8 flex justify-center">
+                    <input className="rounded-md indent-3 py-2 !outline-none" id="sub-input" type="text" value={sub} onChange={e => setSub(e.target.value)} name="category" />
+                    <p className="absolute -top-[1.3rem] left-[0.15rem] text-sm text-gray-300">sub-category</p>
+                </div>
+                <div className="w-1/3 mx-auto relative my-8 flex justify-center">
+                    <textarea placeholder="..." className="rounded-md py-2 px-3 bg-gray-200 !outline-none" value={description} onChange={e => setDescription(e.target.value)} name="description" />
+                    <p className="absolute -top-[1.3rem] left-[0.15rem] text-sm text-gray-300">description</p>
+                </div>
+                <div className="w-2/3 mx-auto bg-gray-300/10 rounded-lg py-5 px-2 flex justify-center mt-4">
+                    <input className="mx-auto w-3/4 text-gray-300 text-sm file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-gray-500 file:text-gray-900" type="file" accept="image/jpeg image/png" name="file"/>
+                </div>
+                <button className="w-1/5 py-1 rounded-full mx-auto my-8 text-md bg-gray-500 hover:bg-gray-700 transition-all duration-300" type="submit">submit</button>
             </form>
+            {error ? 
+            <div className="fixed w-1/5 bg-red-700 rounded-lg h-fit z-40 mx-auto left-[40%] bottom-10">
+                <p className="text-center mx-6 py-3 font-bold font-mono select-none">{error}</p>
+                <p onClick={() => setError(null)} className="absolute top-1 right-3 rotate-45 font-bold text-xl hover:cursor-pointer">+</p>
+            </div>
+            : null }
         </div>
     )
 }
