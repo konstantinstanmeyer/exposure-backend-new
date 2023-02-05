d9dfcb1b4c92df726ad47e09c7eeea0fae1dfc95
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-02-04 21:52:31 -0700

    subcategory page css/routing

--- pages/category/sub/[subName].tsx
+++ pages/category/sub/[subName].tsx
@@ -1,27 +1,49 @@
 import Navbar from '@/components/Navbar'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Sub(){
+    const [category, setCategory] = useState<String | undefined | String[]>("")
+    const [subName, setSubName] = useState<String | undefined | String[]>("");
+
     const router = useRouter();
 
     useEffect(() => {
         console.log(router);
+
+        setCategory(router.query.category);
+        setSubName(router.query.subName);
+
+
     }, [])
 
     return (
         <div className="relative">
             <Navbar />
-            <div className="grid-cols-3 mt-40 mx-auto w-1/2 grid">
-                <Link href={`/post?category=${"yes"}`} className="bg-gray-300 hover:bg-gray-500 transition-all duration-200 w-5/6 h-[15rem] my-4 rounded-md relative flex justify-center items-center">
+            <div className="fixed flex flex-row items-center text-white w-full z-40 top-[7.25rem] h-10 ml-3 select-none">
+                <p className="text-gray-900 mx-auto text-center text-3xl rounded-md font-bold bg-white/50 backdrop-blur px-4 py-1">{`${subName}`}</p>
+            </div>
+            <div className="grid-cols-3 mt-40 mx-auto w-1/2 grid relative pl-5">
+                <Link href={`/post/?category=${category}&sub=${subName}`} className="bg-gray-300 hover:bg-gray-500 transition-all duration-200 w-5/6 h-[15rem] my-8 rounded-md relative flex justify-center items-center mx-auto">
                     <p className="text-center text-6xl font-light">+</p>
                 </Link>
-                <div className="bg-white w-5/6 h-[15rem] my-4 rounded-md" />
-                <div className="bg-white w-5/6 h-[25rem] row-span-2 my-4 rounded-md" />
-                <div className="bg-white w-5/6 h-[25rem] row-span-2 my-4 rounded-md" />
-                <div className="bg-white w-5/6 h-[25rem] row-span-3 my-4 rounded-md" />
-                <div className="bg-white w-5/6 h-[15rem] my-4 rounded-md" />
+                <div className="bg-white w-5/6 h-[15rem] my-8 rounded-md relative mx-auto">
+                    <img src="https://images.unsplash.com/photo-1429087969512-1e85aab2683d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" className="w-full h-full rounded-md" />
+                </div>
+                <div className="bg-white w-5/6 h-[15rem] my-8 rounded-md mx-auto" />
+                <div className="bg-white w-5/6 h-[25rem] row-span-2 my-8 rounded-md mx-auto" />
+                <div className="bg-white w-5/6 h-[25rem] row-span-2 my-8 rounded-md mx-auto" />
+                <div className="bg-white w-5/6 h-[25rem] row-span-2 my-8 rounded-md mx-auto" />
+                <div className="bg-white w-5/6 h-[15rem] my-8 rounded-md mx-auto" />
+                <div className="bg-white w-5/6 h-[15rem] my-8 rounded-md mx-auto" />
+                <div className="bg-white w-5/6 h-[15rem] my-8 rounded-md mx-auto" />
+                <div className="bg-white w-5/6 h-[15rem] my-8 rounded-md mx-auto" />
+                <div className="bg-white w-5/6 h-[25rem] row-span-2 my-8 rounded-md mx-auto" />
+                <div className="bg-white w-5/6 h-[15rem] my-8 rounded-md mx-auto" />
+
+                <div className="bg-white w-5/6 h-[15rem] my-8 rounded-md mx-auto" />
+
             </div>
         </div>
     )
--- pages/post.tsx
+++ pages/post.tsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useRouter } from 'next/router';
 import { ChangeEvent, useState, useEffect } from 'react';
 import { useSelector } from 'react-redux'
+import Navbar from "@/components/Navbar";
 
 async function uploadToS3(e: ChangeEvent<HTMLFormElement>){
     const formData = new FormData(e.target);
@@ -25,7 +26,8 @@ async function uploadToS3(e: ChangeEvent<HTMLFormElement>){
 
 export default function Post(){
     const [genre, setGenre] = useState("");
-    const [category, setCategory] = useState("");
+    const [category, setCategory] = useState<any>("");
+    const [sub, setSub] = useState<any>("");
     const [description, setDescription] = useState("");
     const username = useSelector((state: any) =>  state.auth.username);
     const token = useSelector((state: any) =>  state.auth.token);
@@ -34,6 +36,9 @@ export default function Post(){
 
     useEffect(() => {
         if (!username || !token) router.push('/login');
+
+        setCategory(router.query.category);
+        setSub(router.query.sub);
     }, [])
 
     async function handleSubmit(e: ChangeEvent<HTMLFormElement>){
@@ -53,8 +58,15 @@ export default function Post(){
     }
 
     return (
-        <div>
-            
+        <div className="">
+            <Navbar />
+            <form onSubmit={handleSubmit}>
+                <input type="file" accept="image/jpeg image/png" name="file"/>
+                <input type="text" value={genre} onChange={e => setGenre(e.target.value)} name="genre" />
+                <input type="text" value={category} onChange={e => setCategory(e.target.value)} name="category" />
+                <input type="text" value={description} onChange={e => setDescription(e.target.value)} name="description" />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
