a67430f2fdf6645325b2ce85e05a0bd09edd53dc
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-02-06 11:35:56 -0700

    functional styled slider

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -5,3 +5,23 @@ export default function Admin(){
         </div>
     )
 }
+
+// async function submitCategory(e: ChangeEvent<HTMLFormElement>){
+//     e.preventDefault();
+
+//     const response = await axios.post('http://localhost:3001/add-category', {
+//         // name: category,
+//         // imageUrl: imageUrl,
+//     }, { headers: { "Authorization": "Bearer "+ localStorage.getItem('token')}})
+// }
+
+// async function submitSubCategory(e: ChangeEvent<HTMLFormElement>){
+//     e.preventDefault();
+
+//     const response = await axios.post('http://localhost:3001/add-sub-category', {
+//         // name: category,
+//         // imageUrl: imageUrl,
+//         // category: "Farming",
+//         // obscurity: obscurity
+//     }, { headers: { "Authorization": "Bearer "+ localStorage.getItem('token')}})
+// }
--- pages/category/[categoryName].tsx
+++ pages/category/[categoryName].tsx
@@ -72,9 +72,15 @@ export default function Category(){
 
     return(
         <div className="relative w-screen">
-            <div className="w-1/2 mt-20 grid grid-cols-2 mx-auto relative">
+            <div className="w-1/5 h-10 bg-gray-400 absolute z-40 right-14 -top-10 rounded-xl flex flex-row">
+                <div className="h-full w-full relative flex items-center">
+                    <input id="input" className="accent-slate-500 !outline-none mx-auto w-2/3" type="range" />
+                    <p className="absolute text-gray-300 -bottom-7 text-sm w-full text-center font-bold">obscurity {"(1-5)"}</p>
+                </div>
+            </div>
+            <div className="w-1/2 grid grid-cols-2 mx-auto relative mt-36">
                 {subs.length > 0 ? subs.map((sub, i) => 
-                    <Link href={`/category/sub/${sub.name}/?category=${router.query.categoryName}`} id={`${sub.name + i.toString()}`} className={`h-72 w-72 mt-16 hover:bg-gray-600 transition-all duration-300 ${isLoading ? "animate-pulse bg-gray-600 rounded-full" : null} m-auto relative p-3 rounded-md flex items-center justify-center`}>
+                    <Link href={`/category/sub/${sub.name}/?category=${router.query.categoryName}`} id={`${sub.name + i.toString()}`} className={`h-56 w-56 mb-10 hover:bg-gray-600 transition-all duration-300 ${isLoading ? "animate-pulse bg-gray-600 rounded-full" : null} m-auto relative p-3 rounded-md flex items-center justify-center`}>
                         <p className={`absolute -bottom-7 text-center w-1/2 left-[25%] text-gray-300 ${isLoading ? "text-white/0 bg-gray-600 rounded-lg animate-pulse" : null}`}>{sub.name}</p>
                         {isLoading ? null : <img src={sub.imageUrl} className={`w-full rounded-full object-cover h-full bg-gray-600`} />}
                     </Link>
@@ -88,7 +94,7 @@ export default function Category(){
                     </div>
                 </>}
             </div>
-            <div className="mt-14 bg-gray-800 w-32 h-10 mx-auto rounded-md flex flex-row items-center">
+            <div className="bg-gray-800 mt-4 w-32 h-10 mx-auto rounded-md flex flex-row items-center">
                 <p onClick={() => setPage(page => page-1)} className="mx-auto font-extrabold text-gray-300 hover:cursor-pointer">{'<'}</p>
                 <p className="mx-auto text-gray-300 select-none">{`${page}`}</p>
                 <p onClick={() => setPage(page => page+1)} className="mx-auto font-extrabold text-gray-300 hover:cursor-pointer">{'>'}</p>
