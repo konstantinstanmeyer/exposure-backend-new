7cd9402919f5d5ccd94ebca8d95fcb445a9e2572
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-02-14 17:05:28 -0700

    global types post and subs change

--- types/global.ts
+++ types/global.ts
@@ -1,21 +1,15 @@
-export interface Creator {
-    username: String,
-    imageUrl: string
-}
-
-export interface Post {
-    _id: String,
-    title: String,
-    category: String,
-    subCategory: String,
-    description: String,
-    creator: Creator,
-    imageUrl: string,
-    sizing: Number,
-    date: Date | string,
-}
-
 export interface Category {
     name: String;
     imageUrl: String;
+}
+
+export interface SubCategory {
+    name: String;
+    imageUrl: String;
+    obscurity: Number;
+}
+
+export interface CategorySubs {
+    name: String;
+    subs: Array<SubCategory>;
 }
